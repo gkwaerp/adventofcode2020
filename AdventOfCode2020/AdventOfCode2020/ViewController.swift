5a48cf7d45c68d77c48d64a7aa28e92004ddbbd3
Geir-Kåre S. Wærp <geir-kare.waerp@shortcut.no>
2020-10-21 19:55:06 +0200

    Created project.

---
+++ AdventOfCode2020/AdventOfCode2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdventOfCode2020
+//
+//  Created by Geir-Kåre S. Wærp on 21/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
